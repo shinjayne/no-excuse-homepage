852bec4092ed80ac7ea4c9db55f0737a87472997
jayneshin <shinjayne@gmail.com>
2024-04-12 11:05:09 +0900

    footer 수정

--- app/footer.ts
+++ app/footer.ts
@@ -1,7 +1,7 @@
 
 
 const FooterContent = "" +
-  "          노익스큐즈 | CEO. 배수록, 신재인 | 서울특별시 강남구 개포동 1190-6\n" +
+  "          상호명 노익스큐즈 | 대표자 배수록 | 서울특별시 강남구 개포동 1190-6\n" +
   "          302호 | 제휴/협업. no.excuse.contact@gmail.com | 사업자 등록번호. 507-52-00835 |\n" +
-  "          010-5133-6385"
+  "          연락처 010-5133-6385"
 export default FooterContent
