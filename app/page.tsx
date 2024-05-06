821c804b30047838c15199f77b205799fa74971a
shinjayne <shinjayne@gmail.com>
2024-05-06 20:51:18 +0900

    방어로직 추가

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,19 @@ import { NextUIProvider } from '@nextui-org/react'
 import MobileV2 from '@/app/mobilev2'
 import DesktopV5 from '@/app/desktopv5'
 import { redirect } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Home() {
   const windowSize = useWindowSize()
 
-  if (
-    typeof window != 'undefined' &&
-    window.location.hostname === 'showroom.no-excuse.shop'
-  ) {
-    redirect('https://sixshop.com/no-excuse' + window.location.pathname)
-  }
+  useEffect(() => {
+    if (
+      typeof window != 'undefined' &&
+      window.location.hostname === 'showroom.no-excuse.shop'
+    ) {
+      redirect('https://sixshop.com/no-excuse' + window.location.pathname)
+    }
+  })
 
   if (windowSize.width > 640) {
     console.log('desktop loaded')
