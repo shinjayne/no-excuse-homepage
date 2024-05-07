df2a2218bb3ad7203b45dfe7eef63e73e72496ac
shinjayne <shinjayne@gmail.com>
2024-05-08 00:34:18 +0900

    404 페이지 커스텀

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,22 @@
+'use client'
+
 import { useEffect } from 'react'
 import { redirect } from 'next/navigation'
 
-export default function Custom404() {
+export default function NotFound() {
   useEffect(() => {
     if (window.location.hostname === 'showroom.no-excuse.shop') {
       redirect('https://sixshop.com/no-excuse' + window.location.pathname)
     }
   })
-  
-  return <h1>404 - Page Not Found</h1>
+
+  return (
+    <div
+      className={
+        'flex h-screen w-screen justify-center items-center bg-black text-[#C60000]'
+      }
+    >
+      Not Found
+    </div>
+  )
 }
