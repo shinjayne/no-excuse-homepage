de0e24acc44075bd25c93bc78a61c74e03af1e93
jayneshin <shinjayne@gmail.com>
2024-05-16 11:06:55 +0900

    naver 사이트 소유 확인

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <meta name="naver-site-verification" content="277c753d354cbf1917220136bb1cd5e6a851fb03" />
+    </head>
     <body
       className={manrope.className}
     >{children}</body>
