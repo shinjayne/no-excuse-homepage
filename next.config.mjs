5b81f27c137d654e36267c042b3367e9b2a4fbf8
jayneshin <shinjayne@gmail.com>
2024-05-16 11:04:23 +0900

    naver 사이트 소유 확인

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+  async rewrites(){
+    return [
+      {
+        source: '/naver09e9a46cd65d798de298006f1bd4bdbc.html',
+        destination: '/naver09e9a46cd65d798de298006f1bd4bdbc.html'
+      }
+    ]
+  }
+};
 
 export default nextConfig;
