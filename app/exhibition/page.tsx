cee1ed7edfceafdc0e2c3c0c84d61b6c10332652
obzva <dngyng1000@gmail.com>
2024-06-09 07:53:40 +0900

    bridge pages 생성
    - server component
    - permanentRedirect

---
+++ app/exhibition/page.tsx
@@ -0,0 +1,6 @@
+import { SIX_SHOP_LINKS } from '@/app/menu'
+import { permanentRedirect } from 'next/navigation'
+
+export default async function Page() {
+  permanentRedirect(SIX_SHOP_LINKS.EXHIBITION)
+}
