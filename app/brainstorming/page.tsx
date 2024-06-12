f261e8993ed8aab5d141d261d6544ddb0ca7e171
flynn0909 <flynn@acon3d.com>
2024-06-12 16:24:15 +0900

    wip

--- app/brainstorming/page.tsx
+++ app/brainstorming/page.tsx
@@ -1,12 +1,13 @@
 import { SIX_SHOP_LINKS } from '@/app/menu'
-import { permanentRedirect } from 'next/navigation'
 
-export const metadata = {
-  alternates: {
-    canonical: SIX_SHOP_LINKS.BRAINSTORMING,
-  },
-}
+export const metadata = {}
 
-export default async function Page() {
-  permanentRedirect(SIX_SHOP_LINKS.BRAINSTORMING)
+export default function Page() {
+  return (
+    <div
+      className={'flex items-center justify-center w-screen h-screen bg-black'}
+    >
+      <h1 className={'text-red-600'}>redirecting...</h1>
+    </div>
+  )
 }
