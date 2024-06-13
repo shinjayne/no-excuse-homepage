dc41415797d5b40bb669bf9488c508d4a9321c76
jayneshin <shinjayne@gmail.com>
2024-06-13 14:25:20 +0900

    add google6a723630e4a66f34.html

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
       {
         source: '/naver09e9a46cd65d798de298006f1bd4bdbc.html',
         destination: '/naver09e9a46cd65d798de298006f1bd4bdbc.html'
+      },
+      {
+        source: '/google6a723630e4a66f34.html',
+        destination: '/google6a723630e4a66f34.html'
       }
     ]
   }
