f20ee5ab8c2f05c1620cd2632324badb827ebd50
shinjayne <shinjayne@gmail.com>
2024-08-22 14:00:07 +0900

    exhibition main page

--- app/exhibition/page.tsx
+++ app/exhibition/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 import ogImage from '@/app/og-exhibition.png'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://www.no-excuse-archive.com'),
@@ -25,11 +26,50 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
+      {/* top bar start */}
       <div
-        style={{ width: '100%', marginTop: '-0.5vw' }}
-        className={'flex flex-row justify-center'}
+        style={{
+          width: '100%',
+          // marginTop: '-0.5vw',
+          position: 'fixed',
+          top: '-0.5vw',
+        }}
+        className={'flex flex-col items-center justify-start'}
       >
         <img src={'/assets/noex2_black.png'} style={{ width: '95vw' }} />
+        <div className={'flex justify-center gap-44 mt-8 '}>
+          <Link href={'/exhibition/shop'} className={'text-[16px]'}>
+            SHOP
+          </Link>
+          <Link
+            href={'https://instagram.com/no__ex2'}
+            className={'text-[16px]'}
+          >
+            INSTAGRAM
+          </Link>
+          <Link href={'/archive/project01'} className={'text-[16px]'}>
+            ARCHIVE CENTER
+          </Link>
+        </div>
+      </div>
+      {/*  top bar end */}
+
+      {/* body start */}
+      <div
+        className={
+          'flex flex-col justify-center sm:justify-end w-full h-screen '
+        }
+      >
+        <img src={'/exhibition/photos/DSC04559.jpg'} className={'w-full '} />
+        {/*<div*/}
+        {/*  style={{*/}
+        {/*    width: '100%',*/}
+        {/*    height: '70vh',*/}
+        {/*    backgroundImage: 'url("/exhibition/photos/DSC04559.jpg")',*/}
+        {/*    backgroundSize: 'cover',*/}
+        {/*    backgroundPosition: 'center',*/}
+        {/*  }}*/}
+        {/*></div>*/}
       </div>
     </>
   )
