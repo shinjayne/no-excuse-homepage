e2086e6d6e5e270d076bec28be4e37573b402161
jayneshin <shinjayne@gmail.com>
2024-09-11 22:17:51 +0900

    balaclava 상품 추가

--- app/exhibition/shop/page.tsx
+++ app/exhibition/shop/page.tsx
@@ -12,7 +12,7 @@ function ExhibitionListProduct({
   title: string
   price: string
   subText: string
-  subTextType: 'red' | 'blue'
+  subTextType: 'red' | '#3533a1' | 'orange'
   imgSrc: string
   link: string
 }) {
@@ -20,13 +20,20 @@ function ExhibitionListProduct({
     <Link
       href={link}
       className={
-        'flex flex-col items-center basis-1/2 sm:basis-1/3 text-center text-[11px]'
+        'flex flex-col items-center basis-1/2 sm:basis-1/3 text-center text-[11px] px-1'
       }
     >
       <img src={imgSrc} />
       <div className={'mb-2'}>{title}</div>
       <div className={'mb-2'}>{price}</div>
-      <div className={`text-${subTextType}-800 font-black`}>{subText}</div>
+      <div
+        style={{
+          color: subTextType,
+        }}
+        className={`text-${subTextType}-800 font-black`}
+      >
+        {subText}
+      </div>
     </Link>
   )
 }
@@ -43,10 +50,30 @@ export default function ExhibitionShop() {
         }}
       >
         <ExhibitionListProduct
+          title={'V1.0 SHIRTS BALACLAVA - VIOLET YELLOW'}
+          price={'???'}
+          subText={'DROP AT 9/24 17:00 KST'}
+          subTextType={'#3533a1'}
+          imgSrc={'/exhibition/products/004_balaclava/002.jpg'}
+          link={
+            'https://no9excuse.com/product/45dbbb4f-7b1e-4202-accc-a9dbf2c6e3ce-113'
+          }
+        />
+        <ExhibitionListProduct
+          title={'V1.0 SHIRTS BALACLAVA - IVORY BLUE'}
+          price={'???'}
+          subText={'DROP AT 9/24 17:00 KST'}
+          subTextType={'#3533a1'}
+          imgSrc={'/exhibition/products/004_balaclava/001.jpg'}
+          link={
+            'https://no9excuse.com/product/45dbbb4f-7b1e-4202-accc-a9dbf2c6e3ce'
+          }
+        />
+        <ExhibitionListProduct
           title={'V1.0 EXTRA LONG WRINKLE DENIM'}
           price={'599,000 KRW'}
-          subText={'DROP AT 9/9 17:00 KST'}
-          subTextType={'blue'}
+          subText={'SOLD OUT SOON'}
+          subTextType={'orange'}
           imgSrc={'/exhibition/products/003_wrinklepants/003_1_sq.jpg'}
           link={
             'https://no9excuse.com/product/b570b312-5292-408a-a54c-9465a84be606'
@@ -56,8 +83,8 @@ export default function ExhibitionShop() {
         <ExhibitionListProduct
           title={'V1.0 PROJECT 01 CREW T SHIRTS'}
           price={'89,000 KRW'}
-          subText={''}
-          subTextType={'blue'}
+          subText={'SOLD OUT SOON'}
+          subTextType={'orange'}
           imgSrc={'/exhibition/products/002_crewt/002_1.jpg'}
           link={
             'https://no9excuse.com/product/8d128b0e-6f52-4eb2-a095-8130563869ae-108'
